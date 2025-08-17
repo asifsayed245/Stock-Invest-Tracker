7fde85dc6f89a45a7226d9df60ad395c1f25016b
asifsayed245 <133009571+asifsayed245@users.noreply.github.com>
2025-08-17 20:12:12 +0530

    Add files via upload

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [
-    "./index.html",
-    "./src/react-app/**/*.{js,ts,jsx,tsx}",
-  ],
-  theme: {
-    extend: {},
-  },
-  plugins: [],
-};
+  content: ['./index.html', './src/**/*.{ts,tsx,js,jsx}'],
+  theme: { extend: {} },
+  plugins: []
+}
