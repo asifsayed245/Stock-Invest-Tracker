27c3b367672206673b54351c1177abc091dd9ba5
asifsayed245 <133009571+asifsayed245@users.noreply.github.com>
2025-08-17 19:43:23 +0530

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,14 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { fileURLToPath, URL } from 'node:url'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/',                         // ensures correct asset paths
-  build: {
-    outDir: 'dist',                  // write to dist/
-    emptyOutDir: true,
-  }
+  resolve: {
+    alias: {
+      '@': fileURLToPath(new URL('./src', import.meta.url))
+    }
+  },
+  base: '/',
+  build: { outDir: 'dist', emptyOutDir: true }
 })
