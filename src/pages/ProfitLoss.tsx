42732fbfbd9c85c2cb6d96ebf2c5baee4995355d
asifsayed245 <133009571+asifsayed245@users.noreply.github.com>
2025-08-17 17:25:05 +0530

    Delete src directory

--- src/pages/ProfitLoss.tsx
+++
@@ -1,5 +0,0 @@
-import React from "react";
-
-export default function ProfitLoss() {
-  return <h1>Profit/Loss Explorer</h1>;
-}
--- src/pages/Transactions.tsx
+++
@@ -1,5 +0,0 @@
-import React from "react";
-
-export default function Transactions() {
-  return <h1>Transactions History</h1>;
-}
