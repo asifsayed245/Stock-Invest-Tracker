0f3e4e1f906cd6b1fbea57b02cfafd25c8438428
asifsayed245 <133009571+asifsayed245@users.noreply.github.com>
2025-08-17 17:00:43 +0530

    Delete vite.config.ts

--- vite.config.ts
+++
@@ -1,8 +0,0 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-
-// No worker / Cloudflare plugins here.
-// This is the simplest config that Netlify will build cleanly.
-export default defineConfig({
-  plugins: [react()]
-})
