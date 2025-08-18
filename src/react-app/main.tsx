a4185dda68a3f5f938a6a9a2fe379ae7d7f61b58
asifsayed245 <133009571+asifsayed245@users.noreply.github.com>
2025-08-18 19:55:32 +0530

    Update main.tsx

--- src/react-app/main.tsx
+++ src/react-app/main.tsx
@@ -1,14 +1,11 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import { BrowserRouter } from "react-router-dom";
-import App from "./App";
-import "@/react-app/index.css"; // keep your global styles import
+// src/react-app/main.tsx
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import App from './App'
+import './index.css'
 
-const rootEl = document.getElementById("root")!;
-ReactDOM.createRoot(rootEl).render(
+ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
+    <App />
   </React.StrictMode>
-);
+)
