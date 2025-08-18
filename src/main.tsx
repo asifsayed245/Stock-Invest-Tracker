8d7797a4bcd7f80b68a870010a1ff3fab2bb58a0
asifsayed245 <133009571+asifsayed245@users.noreply.github.com>
2025-08-18 20:03:08 +0530

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,13 +1,16 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import { BrowserRouter } from 'react-router-dom'
-import App from '@/react-app/App'
-import '@/react-app/index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { BrowserRouter } from "react-router-dom";
+import App from "./react-app/App";
+import { SupabaseProvider } from "./react-app/lib/SupabaseProvider";
+import "./react-app/index.css";
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
+ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <BrowserRouter>
-      <App />
+      <SupabaseProvider>
+        <App />
+      </SupabaseProvider>
     </BrowserRouter>
   </React.StrictMode>
-)
+);
